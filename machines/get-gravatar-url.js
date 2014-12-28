5aca152c3a346edf77ec802d86f0d2ab0f06e71b
Irl Nathan <irlnathan@gmail.com>
2014-12-28 01:18:47 -0600

    added all of the gravatar options support

--- machines/get-gravatar-url.js
+++ machines/get-gravatar-url.js
@@ -14,6 +14,22 @@ module.exports = {
     gravatarSize: {
       example: '400',
       description: 'The size of the gravatar (1-2048)'
+    },
+    defaultImage: {
+      example: 'http://example.com/images/avatar.jpg',
+      description: 'The image if there\'s no associated image of email address.'
+    },
+    forceDefaultImage: {
+      example: true,
+      description: 'Force the default image for the gravatar.'
+    },
+    rating: {
+      example: 'g',
+      description: 'The rating level that\'s acceptable for the gravatar image.'
+    },
+    secureRequest: {
+      example: true,
+      description: 'If true will use "https:".'
     }
   },
 
@@ -42,31 +58,77 @@ module.exports = {
   fn: function (inputs, exits) {
 
     var Crypto = require('crypto');
+    var _ = require('lodash');
+    var qs = require('querystring');
+
+    
+    inputs.gravatarSize = inputs.gravatarSize || '';
+
+    inputs.defaultImage = inputs.defaultImage || '';
+
+    
+    if (inputs.defaultImage) {
+      inputs.defaultImage = encodeURIComponent(inputs.defaultImage);
+    }
+
+    inputs.forceDefaultImage = inputs.forceDefaultImage || '';
 
 
+    if (inputs.forceDefaultImage) {
+       inputs.forceDefaultImage = 'y'; 
+      }
 
-    // try {
-    //   return exits.success(Crypto.createHash('md5')
-    //     .update(inputs.emailAddress.toLowerCase().trim())
-    //     .digest("hex"));
-    // } catch(e) {
-    //   return exits.error(e);
-    // }
-  
-   
-      return exits.success(Crypto.createHash('md5')
-        .update(inputs.emailAddress.toLowerCase().trim())
-        .digest("hex"));
+    inputs.rating = inputs.rating || '';
 
-    // var gravatarHash =  Crypto.createHash('md5')
-    //   .update(inputs.emailAddress.toLowerCase().trim())
-    //   .digest("hex");
+    inputs.secureRequest = inputs.secureRequest || '';
 
-    //   return exists.success(gravatarHash);
 
-    // return exits.success(Crypto.createHash('md5')
-    // .update(inputs.emailAddress.toLowerCase().trim())
-    // .digest("hex"));
+    inputs.secureRequest = inputs.secureRequest || '';
+
+    var options = {s: inputs.gravatarSize, d: inputs.defaultImage, f: inputs.forceDefaultImage, rating: inputs.rating};
+
+    // Pick out all keys that have a value
+    var options = _.pick(options, function(val, key) {
+      return val
+      });
+
+    try {
+
+    var gravatarHash = Crypto.createHash('md5')
+      .update(inputs.emailAddress.toLowerCase().trim())
+      .digest("hex");
+
+    } catch (error) {
+
+      return exists.error(error);
+    }
+
+    if (inputs.secureRequest) {
+
+      try {
+        // Show Mike this and ask why it doesn't show up as an error when it's returned via exits.error but shows up
+        // when I log it?
+        // return exits.success('https://www.gravatar.com/avatar/'+gravatarHash+s.stringify(options));
+        return exits.success('https://www.gravatar.com/avatar/'+gravatarHash+'?'+qs.stringify(options));
+      } catch (error) {
+
+      console.log(error);
+        return exits.error(new error(error));        
+      }
+
+    }
+
+      try {
+        // Show Mike this and ask why it doesn't show up as an error when it's returned via exits.error but shows up
+        // when I log it?
+        // return exits.success('https://www.gravatar.com/avatar/'+gravatarHash+s.stringify(options));
+        return exits.success('http://www.gravatar.com/avatar/'+gravatarHash+'?'+qs.stringify(options));
+      } catch (error) {
+
+      console.log(error);
+        return exits.error(new error(error));        
+      }
+
 
   }
 
